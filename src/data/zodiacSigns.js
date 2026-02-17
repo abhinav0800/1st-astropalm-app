4b65a15f03be57a2d7f9e31daf1d0600088f81c5
abhinav0800 <ahiry0800@gmail.com>
2026-02-17 17:53:42 +0530

    Create zodiacSigns.js

---
+++ src/data/zodiacSigns.js
@@ -0,0 +1,5 @@
+export const ZODIAC_DATA = [
+  { name: 'Aries', element: 'Fire', traits: 'Bold, ambitious' },
+  { name: 'Pisces', element: 'Water', traits: 'Intuitive, artistic' }
+  // ... rest
+];
