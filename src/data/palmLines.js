c3ec6c70ae65d2faf8ff9d23ffd80ca95ea58d73
abhinav0800 <ahiry0800@gmail.com>
2026-02-17 17:56:40 +0530

    Create palmLines.js

---
+++ src/data/palmLines.js
@@ -0,0 +1,5 @@
+export const PALM_LINES = [
+  { name: 'Life Line', focus: 'Vitality and path' },
+  { name: 'Heart Line', focus: 'Emotions and love' },
+  { name: 'Head Line', focus: 'Intellect and wisdom' }
+];
