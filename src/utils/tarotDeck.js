1c1183d9d38600e0e0ada8306d492dac767dba84
abhinav0800 <ahiry0800@gmail.com>
2026-02-17 17:38:29 +0530

    Create tarotDeck.js

---
+++ src/utils/tarotDeck.js
@@ -0,0 +1,12 @@
+export const shuffleDeck = (deck) => {
+  const newDeck = [...deck];
+  for (let i = newDeck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newDeck[i], newDeck[j]] = [newDeck[j], newDeck[i]];
+  }
+  return newDeck;
+};
+
+export const drawCards = (deck, count = 3) => {
+  return shuffleDeck(deck).slice(0, count);
+};
