5eacd7344d94788ebb7419e7637c7015cab5cb64
abhinav0800 <ahiry0800@gmail.com>
2026-02-17 17:52:54 +0530

    Create tarotCards.js

---
+++ src/data/tarotCards.js
@@ -0,0 +1,7 @@
+export const TAROT_DECK = [
+  { id: 0, name: "The Fool", arcana: "Major" },
+  { id: 1, name: "The Magician", arcana: "Major" },
+  { id: 2, name: "The High Priestess", arcana: "Major" },
+  { id: 10, name: "Wheel of Fortune", arcana: "Major" }
+  // ...Full 78 cards list
+];
