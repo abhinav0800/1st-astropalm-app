7ee3a46e12dafe3f29788d220f3e3caba0203e96
abhinav0800 <ahiry0800@gmail.com>
2026-02-17 19:09:12 +0530

    Create AnimatedBackground.jsx

---
+++ src/components/common/AnimatedBackground.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AnimatedBackground = () => (
+  <div className="cosmic-bg">
+    <div className="stars" />
+    <div className="twinkling" />
+  </div>
+);
+export default AnimatedBackground;
