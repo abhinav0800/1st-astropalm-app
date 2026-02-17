8359a3b54f97b98c7927de85d8fb73a805b73233
abhinav0800 <ahiry0800@gmail.com>
2026-02-17 18:11:15 +0530

    Create LoadingSpinner.jsx

---
+++ src/components/common/LoadingSpinner.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const LoadingSpinner = () => (
+  <div className="spinner-container">
+    <div className="cosmic-spinner" />
+  </div>
+);
+export default LoadingSpinner;
