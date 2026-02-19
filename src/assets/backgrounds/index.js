14c0678c89159dee1f70a52ea53ddd57a26e78cc
abhinav0800 <ahiry0800@gmail.com>
2026-02-19 17:59:32 +0530

    Create index.js

---
+++ src/assets/backgrounds/index.js
@@ -0,0 +1,6 @@
+export const Backgrounds = {
+  mainCosmic: '/assets/images/bg_cosmic_deep.webp',
+  tarotTable: '/assets/images/bg_tarot_dark.webp',
+  palmScanner: '/assets/images/bg_palm_scanner.webp',
+  overlayStars: '/assets/images/stars_pattern.png'
+};
