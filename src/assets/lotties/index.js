be4e3a9e16b78d153d255a59311a1b885488cb67
abhinav0800 <ahiry0800@gmail.com>
2026-02-19 17:55:00 +0530

    Create index.js

---
+++ src/assets/lotties/index.js
@@ -0,0 +1,9 @@
+// Lottie Animation Exports
+// Note: In production, these JSON files should be in public/animations/
+export const LOTTIE_ANIMATIONS = {
+  loading: '/animations/loading.json',
+  palmScan: '/animations/palm-scan.json',
+  cardShuffle: '/animations/card-shuffle.json',
+  stars: '/animations/stars.json',
+  success: '/animations/success.json'
+};
